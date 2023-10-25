790dfd7a2184f89a3af00a9a53fb36575dc87feb
Venrite <146303521+Venrite@users.noreply.github.com>
2023-10-25 00:23:49 -0400

    set it so if on phone, run different parameters for now

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-	const gameContainer = document.getElementById("game-container"); //gray area to play in 
-	const player = document.getElementById("player"); //player box rn
+	/*
+	alright to make power ups we can take createword() and add an ID parameter and if 0, spawn normal wordboxes, if 1-5 or something it'll spawn a wordbox that correlates to a powerup function.
+	an ID of 1 can be nuke, and it'll just go loop through wordboxes foreach(wordbox) and assign .killed to them all, and increase score for each. could add a visual effect via the function.
+	an id of 2 can be a healing wordbox and provides a heart back(possibly an overflow of hearts even IE hearts>3)
+	an id of 3 can be a slowdown for the words, and all it will have to do is setinterval and change difficulty/boss to a slower speed before bringing it back to normal.
+	an id of 4 can reduce word lengths by changing bosslength/normlength, or even change amount of words in each box.
+	an id of 5 can do ....
+	
+	once the wordboxes have appropriate ID's, you just make it so the event listener checks for "if this word is typed and it has X ID, go to Y action".
+	
+	it's quite a simple game so we can modify parameters to make it more unique, like the word spawning directional cardinality can be modifed to spawn from any side, adding chaos to the game
+	
+	a difficulty slider should be initialzied at the beggining and then call the gameloop(). the slider should be like easy medium hard and we'll set the param according.
+	to make the difficulty change over time we can do a difficulty=difficulty-score*10, or something similar to speed up word generation or normlength=normlength+(score%10). 
+		THE IDEA IS TO NOT MAKE THE GAME LAST TOO LONG, BUT NOT BE SUPER HARD FROM THER START, LIKE A 3-10MINUTE MATCH MAX.
+	
+	sound effects should be added to each type of action accordingly (a nuke should make a boom or something, a right typed word should make a ping, gaining a heart should make a heartbeat or something).
+	
+	a lot of the game hinges on the createword function, and it looks quite simple but we can create supportive functions to make the game feel more like a game and more unique while adding 
+	complexity, like adding an effect of a "bullet" going to the wordBox that is typed and applying the .killed, and then the sound effect can be twofold, the right typed word can make a 
+	gunshot noise, the .killed application could be a hit marker noise or a small impact noise.
+	
+	the boxes for a prototype should be fine however we can either pretty the game up by making the boxes and background grids pretty/neon. or we cna add sprites and images (like squirels falling down are the wordboxes
+	and the background of the webpage can be a forest and the game-container can be a similar forest or a car being protected from wildlife or something goofy, or the game can be meteorites falling down)
+	*/
+	
+	const gameContainer = document.getElementById("game-container"); //gray area to play in being grabbed
+	const player = document.getElementById("player"); //player box rn placeholder incase we want it or need it for like, firing bullets to wordboxes
+	const correctSound = document.getElementById('correctSound');
+	correctSound.volume = 0.1;
+	const lifeloss = document.getElementById('lifeloss');
+	lifeloss.volume = 0.1;
+	const dead = document.getElementById('dead');
+	dead.volume = 0.2;
 	let score = 0;
 	let time = 0;
 	let lives = 3;
@@ -34,12 +66,15 @@
 		}
 	}
 	//function to create and animate a word
-	async function createWord(num, length) { //asynce to use await, add modifier here to make it so if true, match ID and its a powerup when typed.
+	async function createWord(num, length, id) { //asynce to use await, add modifier here to make it so if true, match ID and its a powerup when typed.
 		const wordBox = document.createElement("div"); //make DOM area
 		wordBox.classList.add("word-box"); //give it a wordbox
 		wordBox.style.left = `${Math.random() * 25 + 25}vw`; //give it a position based on viewport width, staying mostly central
 		const randomWord = await fetchWords(num, length); //get word based on 6 length rn
 		wordBox.textContent = randomWord; //prolly put await fetch here
+		if (id!=0){ 
+			wordbox.textcontent=id;
+			}
 		gameContainer.appendChild(wordBox); //add a child node to the game container.
 		//adjust animation duration based on word length
 		const animation = wordBox.animate( //set our animation for the words
@@ -60,8 +95,10 @@
 				//update the heart graphic
 				const heartsContainer = document.getElementById('lives');
 				const hearts = document.querySelectorAll('.heart');
+				lifeloss.play();
 				hearts[lives].remove();
 				if (lives <= 0) { //safer to say lessthan incase any weird bug occurs
+					dead.play();
 					endGame();
 				}
 			}
@@ -70,13 +107,16 @@
 	}
 	//function to end the game
 	function endGame() {
+		setTimeout(() => {
 		alert(`Game Over! Score: ${score}`);
 		location.reload();
+		}, 500);
 	}
 
 	function updateScore() {
 		const scoreElement = document.getElementById("scoreValue");
 		scoreElement.textContent = score;
+		correctSound.play();
 	}
 
 	function updateTimer() { //it turns our clock on
@@ -96,8 +136,7 @@
 			const wordText = wordBox.textContent.trim()
 				.toLowerCase(); //take the target text and make it lowercase
 			if (typedText === wordText) {
-				startTime = new Date()
-					.getTime(); //restart our timer for now
+				startTime = new Date().getTime(); //restart our timer for now
 				wordBox.classList.add("killed"); //add a killed modifer to the object
 				wordBox.style.backgroundColor = "#0f0"; //change color to green when killed
 				wordBox.style.animation = "shake 0.5s"; //apply the shake animation
@@ -115,14 +154,24 @@
 		if (lives === 3) {
 			createHearts();
 		}
-		startTime = new Date()
-			.getTime(); //starts timer
+		startTime = new Date().getTime(); //starts timer
 		updateTimer(); //literally just turns our clock on
+		if (window.innerWidth < 600) {
+			setInterval(() => {
+			createWord(normnum, normlength,0);
+		}, difficulty);
+		}
+		else{
 		setInterval(() => {
-			createWord(normnum, normlength);
+			createWord(normnum, normlength,0);
 		}, difficulty); //adjust the interval for word creation
 		setInterval(() => {
-			createWord(bossnum, bosslength);
+			createWord(bossnum, bosslength,0);
 		}, boss); //adjust the interval for word creation
+		setInterval(() => {
+			Randid=Math.floor(Math.random() * (5 - 1 + 1)) + 1
+			createWord(normnum, normlength,3);
+		}, 1000);
+		}
 	}
 	gameLoop(); //this is here to start the game, can be moved to later as a start button
